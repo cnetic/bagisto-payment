60e13f871182a2891abeb8a3235556c27c98295d
darek <dariusz.mecinski@gmail.com>
2019-11-19 20:31:19 +0100

    refactor

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,9 +2,12 @@
 
 Route::group(['middleware' => ['web']], function () {
     Route::prefix('admin')->group(function () {
-
-Route::get('configuration/sales/paymentmethods', 'CNetic\Payment\Http\Controllers\ConfigurationController@index')->defaults('_config', [
-    'view' => 'admin::configuration.index'
-])->name('cnetic.payment.configuration.index');
-});
+        Route::get('configuration/{slug?}/{slug2?}', 'CNetic\Payment\Http\Controllers\ConfigurationController@index')
+->defaults('_config', ['view' => 'admin::configuration.index'])
+->where('slug', 'sales')
+->where('slug2', 'paymentmethods')
+->name('cnetic.payment.configuration.index');
+//->parameter('slug', 'configure')
+//->parameter('slug2', 'paymentmethods');
+    });
 });
